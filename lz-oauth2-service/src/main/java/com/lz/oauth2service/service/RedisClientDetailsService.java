366c0756e40716cc6fa52bcb6b444995238c9af6
liangzhong.tan <liangzhong_tan@jt-info.com.cn>
2020-10-19 09:22:03 +0800

    处理密码错误异常

--- lz-oauth2-service/src/main/java/com/lz/oauth2service/service/RedisClientDetailsService.java
+++ lz-oauth2-service/src/main/java/com/lz/oauth2service/service/RedisClientDetailsService.java
@@ -1,9 +1,11 @@
 package com.lz.oauth2service.service;
 
 import com.lz.oauth2service.constants.SecurityConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
 import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 
 import javax.sql.DataSource;
@@ -15,6 +17,7 @@ import javax.sql.DataSource;
  * @version 1.1, 2020/10/15 14:49
  * @see JdbcClientDetailsService
  */
+@Slf4j
 public class RedisClientDetailsService extends JdbcClientDetailsService {
     public RedisClientDetailsService(DataSource dataSource) {
         super(dataSource);
@@ -23,6 +26,9 @@ public class RedisClientDetailsService extends JdbcClientDetailsService {
     @Override
     @Cacheable(value = SecurityConstants.OAUTH_CLIENT_CACHE, key = "#clientId", unless = "#result == null")
     public ClientDetails loadClientByClientId(String clientId) throws InvalidClientException {
-        return super.loadClientByClientId(clientId);
+        log.info("clientId: {}", clientId);
+        BaseClientDetails details = (BaseClientDetails) super.loadClientByClientId(clientId);
+        details.setClientSecret(SecurityConstants.BCRYPT + details.getClientSecret());
+        return details;
     }
 }
